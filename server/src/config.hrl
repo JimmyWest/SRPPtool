d671258cf1181b6ca1919f6ee8448d71cbd0000a
JimmyWest <git@jimmywest.se>
2014-11-25 18:14:14 +0100

    Logger update (lagacy compatible)
    Updates the logger with several new features.
     - Color highlighted text in terminal for different messages.
     - Smarter macros for logging, includes module and line information to the log.
     - Some terminalogy corrections on variables.
     - Other small minor fixes.
    
    Highlight colors can be set in the config.hrl file.

--- server/src/config.hrl
+++ server/src/config.hrl
@@ -1,7 +1,22 @@
 %% Log Logger
 -define(debug, true).
+-define(DATE_SEP, "").
+-define(TIME_SEP, "").
+-define(DATE_TIME_SEP, "-").
+% ANSI escape codes for the logger
+-define(INFO_COLOR, "\033[39;49m").
+-define(ERROR_COLOR, "\033[97;101;1m").
+-define(DEBUG_COLOR, "\033[92;1m").
+-define(UNKOWN_COLOR, "\033[93m").
+-define(END_COLOR, "\033[0m").
+% Logger use of other modules
+-define(LOG_MFL, {?MODULE, ?LINE}).
+-define(LOG_MACRO(Type,Msg), log:Type(?LOG_MFL, Msg)).
+-define(log_info(Msg), ?LOG_MACRO(info,Msg)).
+-define(log_error(Msg), ?LOG_MACRO(err,Msg)).
+-define(log_debug(Msg), ?LOG_MACRO(debug,Msg)).
 
 %% Port handler
--define(GEN_TCP_CONF, [binary, {packet, 0}, {active, true}]).
+-define(GEN_TCP_CONF, [binary, {packet, 0}, {active, false}]).
 -define(ACCEPT_TIMEOUT, 1000).
 -define(SOCKET_COOLDOWN_TIMEOUT, 2000).
--- server/src/log.erl
+++ server/src/log.erl
@@ -1,14 +1,40 @@
 -module(log).
 
--export([start/1, info/1, debug/1, err/1]).
+-export([start/1, stop/0, info/2, info/1, debug/2, debug/1, err/2, err/1]).
 
 -include("config.hrl").
 
 start(_) ->
     Pid = spawn(fun() -> init() end),
-    common:safe_register(logger, Pid),
     Pid.
 
+stop() ->
+    logger ! stop.
+
+info(Info, Msg) ->
+    log(info, Info, Msg).
+info(Msg) ->
+    log(info, Msg).
+
+debug(Info, Msg) ->
+    log(debug, Info, Msg).
+debug(Msg) ->
+    case ?debug of
+	true ->
+	    log(debug, Msg);
+	_ ->
+	    ok
+    end.
+err(Info,Msg) ->
+    log(error,Info,Msg).
+err(Msg) ->
+    log(error, Msg).
+
+log(Type, Info, Msg) ->
+    common:send_singleton(looger, common:mfa(log,start,[]), {log, Type, Info, Msg}).
+
+log(Type, Msg) ->
+    common:send_singleton(logger, common:mfa(log,start,[]), {log, Type, Msg}).
 
 init() ->
     recv_loop().
@@ -18,6 +44,9 @@ recv_loop() ->
 	{log, Type, Msg} ->
 	    log_data(Type, Msg),
 	    recv_loop();
+	{log, Type, Info, Msg} ->
+	    log_data(Type, [Info,", "|Msg]),
+	    recv_loop();
 	stop ->
 	    recv_loop_end()
     end.
@@ -26,6 +55,9 @@ recv_loop_end() ->
     receive
 	{log, Type, Msg} ->
 	    log_data(Type, Msg),
+	    recv_loop_end();
+	{log, Type, Info, Msg} ->
+	    log_data(Type, [Info|Msg]),
 	    recv_loop_end()
     after 0 -> ok
     end.
@@ -34,13 +66,13 @@ log_data(Type, Msg) when is_list(Msg) ->
     print_info(),
     case Type of
 	info ->
-	    print_ln(["I: "|Msg]);
+	    print_ln([?INFO_COLOR, "I: "|Msg],true);
 	error ->
-	    print_ln(["E: "|Msg]);
+	    print_ln([?ERROR_COLOR, "err: "|Msg], true);
 	debug ->
-	    print_ln(["DEBUG: "|Msg]);
+	    print_ln([?DEBUG_COLOR, "DEBUG: "|Msg], true);
 	_ ->
-	    print_ln([Type,": "|Msg])
+	    print_ln([Type,": "|Msg],false)
     end;
 log_data(Type, Msg) ->
     log_data(Type, [Msg]).
@@ -51,7 +83,7 @@ print_info() ->
 print_date() ->
     {Y,M,D} = date(),
     {H,Mi,S} = time(),
-    print(["[",Y,"-",z(M),"-",z(D),"](",z(H),":",z(Mi),":",z(S),")"]).
+    print(["[",Y,?DATE_SEP,z(M),?DATE_SEP,z(D),?DATE_TIME_SEP,z(H),?TIME_SEP,z(Mi),?TIME_SEP,z(S),"]"]).
 
 z(V) when V < 10 ->
     "0" ++ integer_to_list(V);
@@ -59,9 +91,14 @@ z(V) ->
     V.
 
 
-print_ln(Data) ->
+print_ln(Data, Color) ->
     print(Data),
-    io:format("~n").
+    case Color of
+	true->
+	    io:format("~s~n",[?END_COLOR]);
+	_ ->
+	    io:format("~n")
+    end.
 
 print([]) -> ok;
 print([H|T]) when is_list(H) ->
@@ -70,20 +107,3 @@ print([H|T]) when is_list(H) ->
 print([H|T]) ->
     io:format("~p",[H]),
     print(T).
-
-info(Info) ->
-    log(info, Info).
-
-debug(Info) ->
-    case ?debug of
-	true ->
-	    log(debug, Info);
-	_ ->
-	    ok
-    end.
-
-err(Info) ->
-    log(error, Info).
-
-log(Type, Msg) ->
-   common:send_singleton(logger, common:mfa(log,start,[]), {log, Type, Msg}).
