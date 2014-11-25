4b77edc93781c8f7fc42472fed8e9543648220a0
JimmyWest <git@jimmywest.se>
2014-11-26 00:31:02 +0100

    Adding safe sending functions and optional response functions.

--- server/src/common.erl
+++ server/src/common.erl
@@ -1,6 +1,6 @@
 -module(common).
 
--export([safe_register/2, mfa/3, send_singleton/3, send_sync/2, send/2, response/1, receive_msg/1, reply/2]).
+-export([safe_register/2, mfa/3, send_singleton/3, safe_send_sync/2, safe_send/2, send_sync/2, send/2, response/2, response/1, response/0, receive_msg/1, reply/2]).
 
 safe_register(Name, Pid) ->
     case erlang:whereis(Name) of
@@ -26,6 +26,43 @@ send_singleton(Name, {M,F,A}, Msg) ->
     Pid ! Msg,
     ok.
 
+safe_send_sync(Dest, Msg) ->
+    case isalive(Dest) of
+	fasle ->
+	    Ref = echo(stopped),
+	    response(Ref);
+	_ ->
+	    send_sync(Dest, Msg)
+    end.
+
+safe_send(Dest, Msg) ->
+    case isalive(Dest) of
+	false ->
+	    echo(stopped);
+	_ ->
+	    send(Dest, Msg)
+    end.
+
+isalive(Dest) when is_atom(Dest) ->
+    case erlang:whereis(Dest) of
+	undefined ->
+	    false;
+	_ -> true
+    end;
+isalive(Dest) when is_pid(Dest) ->
+    case erlang:process_info(Dest) of
+	undefined ->
+	    false;
+	_ -> true
+    end;
+isalive(_) ->
+    false.
+
+echo(Msg) ->
+    Ref = make_ref(),
+    self() ! {response, Ref, Msg},
+    Ref.
+
 send_sync(Destination, Msg) ->
     Ref = send(Destination, Msg),
     response(Ref).
@@ -35,10 +72,22 @@ send(Destination, Msg) ->
     Destination ! {msg, self(), Ref, Msg},
     Ref.
 
+response(Ref, Timeout) ->
+    receive
+	{response, Ref, Msg} ->
+	    Msg
+    after Timeout -> timeout
+    end.
+
 response(Ref) ->
     receive
 	{response, Ref, Msg} ->
 	    Msg
+    end.
+
+response() ->
+    receive
+	{response, _, Msg} -> Msg
     end.
 
 receive_msg(unlimited) ->
