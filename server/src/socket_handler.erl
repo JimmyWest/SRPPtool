bd2443226b896013bee0a67e3eb69c3a401581a7
JimmyWest <git@jimmywest.se>
2014-12-03 13:47:58 +0100

    Removeing logger legacy code in socket_handler.

--- server/src/socket_handler.erl
+++ server/src/socket_handler.erl
@@ -2,6 +2,8 @@
 
 -export([start/2, response/3]).
 
+-include("config.hrl").
+
 start(Socket, Client) ->
     spawn(fun() -> init(Socket, Client) end).
 
@@ -13,8 +15,7 @@ response(Socket, Type, Msg) ->
     ok.
 
 init(Socket, Client) ->
-    log:info(["New connection initialized ..."]),
-    % erlang:monitor(process,Client),
+    ?log_info(["New connection initialized ..."]),
     recv_loop(Socket, Client).
 
 recv_loop(Socket, Client) ->
@@ -28,10 +29,10 @@ recv_loop(Socket, Client) ->
     end.
 
 failure_recovery(closed, _, Client) ->
-    log:info(["Client(",Client,") socket closed!"]),
+    ?log_info(["Client(",Client,") socket closed!"]),
     client_handler:socket_closed(Client);
 failure_recovery(Reason, Socket, Client) ->
-    log:err(["Client(",Client,") socker error, due to: ",Reason]),
+    ?log_error(["Client(",Client,") socker error, due to: ",Reason]),
     client_handler:socket_closed(Client),
     gen_tcp:close(Socket).
 
