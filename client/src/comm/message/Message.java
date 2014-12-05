1759412a3079d3f0a922d1be0564c87d07ec5f30
JimmyWest <git@jimmywest.se>
2014-12-05 16:22:31 +0100

    #12, Messages super classes for message content and message type.

--- client/src/comm/message/Message.java
+++ client/src/comm/message/Message.java
@@ -6,15 +6,29 @@ package comm.message;
 public class Message {
 
     private MessageType type;
+    private static byte counter = 0;
+    private byte id;
     private byte length;
     private byte[] data;
 
-    public Message(MessageType type) {
-        this.type = type;
-    }
+    public Message(MessageType type) { this(type,null); }
     public Message(MessageType type, byte[] data) {
+        counter();
         this.type = type;
         this.data = data;
+    }
+    public Message(byte id, MessageType type) {
+        this(id,type,new byte[0]);
+    }
+    public Message(byte id, MessageType type, byte[] data){
+        this.id = id;
+        this.type = type;
+        this.data = data;
+    }
+
+    private synchronized void counter() {
+        this.id = counter;
+        counter++;
     }
 
     public void setType(MessageType type){
@@ -22,17 +36,22 @@ public class Message {
     }
 
     public void setData(byte[] data) {
-        // TODO: Add exeption on length over 255.
+        // TODO: Add exception on length over 255.
         this.length = (byte)data.length;
         this.data = data;
     }
 
+    public byte getId() {
+        return id;
+    }
+
     public byte[] getMessage(){
-        int size = ((int)((2 + data.length)/8)+1)*8;
+        int size = ((int)((3 + data.length)/8)+1)*8;
         byte[] msg = new byte[size];
         msg[0] = type.getCode();
-        msg[1] = length;
-        System.arraycopy(data,0,msg,2,length);
+        msg[1] = id;
+        msg[2] = length;
+        System.arraycopy(data,0,msg,3,length);
         return msg;
     }
 
--- client/src/comm/message/MessageType.java
+++ client/src/comm/message/MessageType.java
@@ -4,7 +4,23 @@ package comm.message;
  * Created by jimmywest on 2014-12-04.
  */
 public enum MessageType {
-    CONNECT(1), DISCONNECT(2);
+
+    // Connection request message
+    CONNECT(1), DISCONNECT(2),
+
+    // Main request messages
+    FOLDER(10),
+
+    // File request message
+    FILE_OPEN(100), FILE_SUBSCRIBE(101), FILE_CONTENT(110),FILE_LINE_GET(111),
+    FILE_LINE_NEW(120),FILE_LINE_REMOVE(121),FILE_LINE_UPDATE(122),
+    FILE_CURSOR_POS(150),
+
+    // Response message types
+    OK(200), ACK(201), ERROR(250),
+
+    // Unknown Type
+    UNKNOWN(0);
 
     private final byte code;
     MessageType(int code){this.code = (byte)code;}
